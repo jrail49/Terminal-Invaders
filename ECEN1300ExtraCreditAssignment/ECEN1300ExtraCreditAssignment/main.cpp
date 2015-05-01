70a90ae3fda22ef111400dd2f70574cf146e9094
Jacob Rail <jacot.rail@colorado.edu>
2015-04-30 21:43:37 -0600

    initial commit

---
+++ ECEN1300ExtraCreditAssignment/ECEN1300ExtraCreditAssignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECEN1300ExtraCreditAssignment
+//
+//  Created by Jacob Rail on 4/30/15.
+//  Copyright (c) 2015 CU Boulder. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
